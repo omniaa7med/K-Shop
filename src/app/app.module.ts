18a8ccaea43e5795ffce2d4d309fff0b2e2cc39b
omniaa7med <omniaahmed71098@gmail.com>
2022-07-30 20:49:06 +0200

    'create components'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,

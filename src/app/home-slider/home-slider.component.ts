f6854bb6ea721ff44b1315255f6e9d9ddefebc01
omniaa7med <omniaahmed71098@gmail.com>
2022-07-31 23:24:17 +0200

    'add slider'

--- src/app/home-slider/home-slider.component.ts
+++ src/app/home-slider/home-slider.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-slider.component.scss']
 })
 export class HomeSliderComponent implements OnInit {
+  slideIndex = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.showSlides(this.slideIndex);
   }
 
+
+  plusSlides(n: any) {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  /* ---------------------------------------------------------------- */
+  /*                         slider Function                          */
+  /* ---------------------------------------------------------------- */
+
+  showSlides(n: any) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides") as HTMLCollectionOf<HTMLElement>;
+    if (n > slides.length) { this.slideIndex = 1 }
+    if (n < 1) { this.slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[this.slideIndex - 1].style.display = "block";
+  }
 }

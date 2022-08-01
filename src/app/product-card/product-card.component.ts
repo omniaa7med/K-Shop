39a6bde1588e218af067c1e7f629f21db8ac7328
omniaa7med <omniaahmed71098@gmail.com>
2022-08-01 04:04:19 +0200

    'add product visited card'

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -4,20 +4,11 @@ import { Product } from '../interfaces/product';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
   @Input() product?: Product;
-  stars: Array<number> = [1, 2, 3, 4, 5]
-  proStar: Array<object> =[]
-  constructor() {
+  constructor() {}
 
-  }
-
-  ngOnInit(): void {
-
-
-  }
-
-
+  ngOnInit(): void {}
 }
